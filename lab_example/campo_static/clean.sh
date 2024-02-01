a42ddd3a901f3b5cf8cc113be89cb2fda33883df
Natalia Kurdanova <kurdanova.natalia@outlook.com>
2024-02-01 12:58:56 +0100

    .

--- lab_example/campo_static/clean.sh
+++
@@ -1,3 +0,0 @@
-rm -rf food_environment.lue
-rm -rf utrecht.pdf utrecht.pdf.aux.xml agents.csv
-rm -rf agents_operations.csv
--- lab_example/campo_static/plot_point_objects.py
+++
@@ -1,6 +0,0 @@
-import lue.data_model as ldm
-import campo
-
-dataset = ldm.open_dataset('food_environment.lue')
-
-dataframe = campo.dataframe.select(dataset.foodstores, property_names=['x_initial'])
